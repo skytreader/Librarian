e9aa84032002bd2fac01ff70dfc49adaa653ce54
root <root@chad-laptop.(none)>
2012-04-13 18:19:49 +0800

    Issue#1 (concerning Dashboard) resolution as discussed.

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -20,22 +20,27 @@ class Dashboard extends CI_Controller{
 	*/
 	public function index(){
 		$this->load->model("LoginModel");
-		if($this->LoginModel->verify($_POST["username"], $_POST["password"])){
+		$this->load->library("session");
+		
+		$is_logged_in = $this->session->userdata(SESSION_LOGGED_IN);
+		
+		if(!$is_logged_in && $this->LoginModel->verify($_POST["username"], $_POST["password"])){
 			$user_session[SESSION_USERNAME] = $_POST["username"];
 			$user_session[SESSION_LOGGED_IN] = TRUE;
 			
 			$this->load->library("session");
 			$this->session->set_userdata($user_session);
-			
+		} else{
+			//Just load some views here.
+		}
+		
+		if($is_logged_in){
 			$page_data["echo_content"] = TRUE;
 			$page_data["content"] = "<h1>Welcome to your Dashboard</h1>";
 			$page_data["title"] = "Dashboard";
 			$page_data["logged_in"] = TRUE;
 			$this->load->helper("url");
 			$this->load->view("mainview", $page_data);
-		} else{
-			//Just load some views here.
-			echo "Log-in failed.";
 		}
 	}
 	
