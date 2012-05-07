8ec4cd16c5e6d9ed08c07bef45ab9b5357bff056
root <root@chad-laptop.(none)>
2012-05-08 00:29:31 +0800

    Greatly simplified my views with <base>. Love you <base>!

--- application/views/mainview.php
+++ application/views/mainview.php
@@ -17,11 +17,12 @@ to determine if we should display the logged-in navbar.
 <html>
 	<head>
 		<title><?php echo APP_TITLE . " - " . $title; ?></title>
-		<link rel="stylesheet" type="text/css" href="<?php echo base_url() . 'css/librariantheme.css';?>" />
+		<base href="<?php echo base_url(); ?>" />
+		<link rel="stylesheet" type="text/css" href="css/librariantheme.css" />
 	</head>
 	<body>
 		<div class="main_box">
-			<img src="<?php echo base_url();?>images/librarian.png" />
+			<img src="images/librarian.png" />
 			<?php include("navbox.php"); ?>
 			<div class="content_box">
 				<?php
--- application/views/navbox.php
+++ application/views/navbox.php
@@ -1,15 +1,13 @@
 <div class="navbox">
-	<a href="<?php echo base_url() . 'index.php/home'; ?>" class="navbox">Search</a>
-	<a href="<?php echo base_url() . 'index.php/about'; ?>" class="navbox">About</a>
+	<a href="index.php/home" class="navbox">Search</a>
+	<a href="index.php/about" class="navbox">About</a>
 	<?php if(isset($logged_in) && $logged_in){ ?>
-		<a href="<?php echo base_url() . 'index.php/logout'; ?>" class="navbox">Log Out</a>
-		<a href="<?php echo base_url() . 'index.php/dashboard'; ?>" class="navbox">Dashboard</a>
-		<a href="manage/users" class="navbox">Manage Users</a>
-		<a href="manage/books" class="navbox">Manage Books</a>
-		<a href="<?php echo base_url() . 'index.php/settings'; ?>" class="navbox">Account Settings</a>
+		<a href="index.php/logout" class="navbox">Log Out</a>
+		<a href="index.php/dashboard" class="navbox">Dashboard</a>
+		<a href="index.php/manage/users" class="navbox">Manage Users</a>
+		<a href="index.php/manage/books" class="navbox">Manage Books</a>
+		<a href="index.php/settings" class="navbox">Account Settings</a>
 	<?php }else{ ?>
-		<!--No need to call base_url() here since we are sure that this will only appear iff
-		the user isn't logged-in, and therefore the "subdirectoried" URLs are not accessible-->
-		<a href="login" class="navbox">Log In</a>
+		<a href="index.php/login" class="navbox">Log In</a>
 	<?php } ?>
 </div>
