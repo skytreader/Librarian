58daa6744dabdf2ebed7de276777502ed5efebd6
skytreader <chadestioco@gmail.com>
2012-08-18 01:56:50 +0800

    Settings view fix.

--- application/views/content/settings.php
+++ application/views/content/settings.php
@@ -2,8 +2,8 @@
 Expects the variable $app_settings defined containing all the
 rows in the appsettings table in an array.
 -->
-<?php if(isset($settings)): ?>
-	<?php foreach($settings as $s): ?>
+<?php if(isset($app_settings)): ?>
+	<?php foreach($app_settings as $s): ?>
 		<strong><?= $s["settingstring"] ?>:</strong><br />
 		<em><?= $s["description"] ?></em><br />
 		<input type="text" value="<?= $s['settingvalue'] ?>" /><br />
