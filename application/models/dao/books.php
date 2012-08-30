7d156e20257f5416d0c026b1798ccddac2890bb0
skytreader <chadestioco@gmail.com>
2012-08-30 23:02:25 +0800

    Added year field in books DAOModel.

--- application/models/dao/books.php
+++ application/models/dao/books.php
@@ -5,11 +5,13 @@ require_once("bookrelated.php");
 class Books extends BookRelated{
 	
 	const TITLE = "title";
+	const YEAR = "year";
 	
 	public function __construct(){
 		parent::__construct();
 		$this->table_name = "books";
 		$this->fields[Books::TITLE] = null;
+		$this->fields[Books::YEAR] = null;
 		array_push($this->primary_keys, Books::ISBN);
 	}
 	
@@ -19,6 +21,14 @@ class Books extends BookRelated{
 	
 	public function set_title($t){
 		$this->fields[Books::TITLE]= $t;
+	}
+	
+	public function get_year(){
+		return $this->fields[Books::YEAR];
+	}
+	
+	public function set_year($year){
+		$this->fields[Books::YEAR] = $year;
 	}
 }
 
