82568e32e45c4bec01d61fc8ddc1c0560c53f4f5
skytreader <chadestioco@gmail.com>
2012-09-07 01:14:06 +0800

    Created roles DAO--- Look-up table in DB, For authors and like.

---
+++ application/models/dao/roles.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once("daomodel.php");
+
+class Roles extends Daomodel{
+	
+	const ROLEID = "roleid";
+	const ROLENAME = "rolename";
+	
+	public function __construct(){
+		$this->fields[Roles::ROLEID] = null;
+		$this->fields[Roles::ROLENAME] = null;
+		array_push($this->primary_keys, Roles::ROLEID);
+	}
+	
+	public function get_roleid(){
+		return $this->fields[Roles::ROLEID];
+	}
+	
+	public function set_roleid($roleid){
+		$this->fields[Roles::ROLEID] = $roleid;
+	}
+	
+	public function get_rolename(){
+		return $this->fields[Roles::ROLENAME];
+	}
+	
+	public function set_rolename($rolename){
+		$this->fields[Roles::ROLENAME] = $rolename;
+	}
+	
+}
+
+?>
